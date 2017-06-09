ca84bd91fb8c262c37e9ec9bcc59033a69c49aa1
Ed Wei <ed@highfive.com>
2017-06-09 10:10:05 -0700

    Refactoring style

--- TDDMicroExercises/python/TirePressureMonitoringSystem/test_tire_pressure_monitoring.py
+++ TDDMicroExercises/python/TirePressureMonitoringSystem/test_tire_pressure_monitoring.py
@@ -12,46 +12,63 @@ HIGH_PRESSURE_THRESHOLD = 21.0
 # See http://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
 EPSILON = HIGH_PRESSURE_THRESHOLD * sys.float_info.epsilon
 
+class SensorTest(unittest.TestCase):
+
+    def test_float_psi_value(self):
+        sensor = Sensor()
+        psi_value = sensor.pop_next_pressure_psi_value()
+        self.assertTrue(isinstance(psi_value, float))
+
 class AlarmTest(unittest.TestCase):
 
-  def create_sensor(self, next_psi_value):
-    sensor = MagicMock(spec=Sensor)
-    sensor.pop_next_pressure_psi_value.return_value = next_psi_value
-    return sensor
+    def create_sensor(self, next_psi_values):
+        sensor = MagicMock(spec=Sensor)
+        sensor.pop_next_pressure_psi_value.side_effect = next_psi_values
+        return sensor
 
-  def create_alarm(self, next_psi_value = LOW_PRESSURE_THRESHOLD):
-    alarm = Alarm(
-        self.create_sensor(next_psi_value),
-        LOW_PRESSURE_THRESHOLD,
-        HIGH_PRESSURE_THRESHOLD)
-    return alarm
+    def create_alarm(self, next_psi_values):
+        alarm = Alarm(
+            self.create_sensor(next_psi_values),
+            LOW_PRESSURE_THRESHOLD,
+            HIGH_PRESSURE_THRESHOLD)
+        return alarm
 
-  def create_checked_alarm(self, next_psi_value = LOW_PRESSURE_THRESHOLD):
-    alarm = self.create_alarm(next_psi_value)
-    alarm.check()
-    return alarm
+    def create_checked_alarm(self, next_psi_values):
+        alarm = self.create_alarm(next_psi_values)
+        alarm.check()
+        return alarm
 
-  def test_default_alarm_state(self):
-    alarm = self.create_alarm()
-    self.assertFalse(alarm.is_alarm_on)
+    def test_default_alarm_state(self):
+        alarm = self.create_alarm([ LOW_PRESSURE_THRESHOLD ])
+        self.assertFalse(alarm.is_alarm_on)
 
-  def test_low_pressure_threshold(self):
-    alarm = self.create_checked_alarm(LOW_PRESSURE_THRESHOLD)
-    self.assertFalse(alarm.is_alarm_on)
+    def test_low_pressure_threshold(self):
+        alarm = self.create_checked_alarm([ LOW_PRESSURE_THRESHOLD ])
+        self.assertFalse(alarm.is_alarm_on)
 
-  def test_high_pressure_threshold(self):
-    alarm = self.create_checked_alarm(HIGH_PRESSURE_THRESHOLD)
-    self.assertFalse(alarm.is_alarm_on)
+    def test_high_pressure_threshold(self):
+        alarm = self.create_checked_alarm([ HIGH_PRESSURE_THRESHOLD ])
+        self.assertFalse(alarm.is_alarm_on)
 
-  def test_less_than_low_pressure_threshold(self):
-    alarm = self.create_checked_alarm(
-      LOW_PRESSURE_THRESHOLD - EPSILON)
-    self.assertTrue(alarm.is_alarm_on)
+    def test_less_than_low_pressure_threshold(self):
+        alarm = self.create_checked_alarm(
+            [ LOW_PRESSURE_THRESHOLD - EPSILON ])
+        self.assertTrue(alarm.is_alarm_on)
 
-  def test_greater_than_low_pressure_threshold(self):
-    alarm = self.create_checked_alarm(
-      HIGH_PRESSURE_THRESHOLD + EPSILON)
-    self.assertTrue(alarm.is_alarm_on)
+    def test_greater_than_low_pressure_threshold(self):
+        alarm = self.create_checked_alarm(
+            [ HIGH_PRESSURE_THRESHOLD + EPSILON ])
+        self.assertTrue(alarm.is_alarm_on)
+
+    def test_alarm_stays_on_after_returning_to_normal(self):
+        alarm = self.create_alarm([
+            HIGH_PRESSURE_THRESHOLD + EPSILON,
+            HIGH_PRESSURE_THRESHOLD
+            ])
+        alarm.check()
+        self.assertTrue(alarm.is_alarm_on)
+        alarm.check()
+        self.assertTrue(alarm.is_alarm_on)
 
 if __name__ == "__main__":
   unittest.main()
